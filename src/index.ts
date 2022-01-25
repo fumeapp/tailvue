413d729e372d6652fbe4b27db17617dd43e34568
tcampbPPU <tcamp022@gmail.com>
2022-01-24 23:17:37 -0500

    add form toggle themes and sizes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { App, Plugin } from 'vue';
 import PushButton from './PushButton.vue'
+import FormToggle from './FormToggle.vue'
 import ModalBase from './ModalBase.vue'
 import { useModal } from './modal'
 import { useToast } from './toast'
 
 const install:Plugin = (app: App) => {
   app.component('PushButton', PushButton)
+  app.component('FormToggle', FormToggle)
   app.component('ModalBase', ModalBase)
 }
 
-export { PushButton, ModalBase, useModal, useToast, install }
+export { PushButton, FormToggle, ModalBase, useModal, useToast, install }
