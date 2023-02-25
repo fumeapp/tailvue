92ee8235b78e271582c0a460df13a2b53836841d
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2023-02-25 17:19:20 +0545

    fix: use right attrribute name

--- src/toast.ts
+++ src/toast.ts
@@ -4,8 +4,8 @@ import { ComponentOptionsWithObjectProps } from 'vue'
 import { TailvueToast, ToastProps } from './types/toast'
 
 const outerClasses = [
-  'z-40', 'fixed',  'inset-0', 'flex',  'sm:items-start',
-  'px-4', 'py-6',  'pointer-events-none',  'sm:p-6',
+  'z-40', 'fixed', 'inset-0', 'flex', 'sm:items-start',
+  'px-4', 'py-6', 'pointer-events-none', 'sm:p-6',
 ]
 
 const innerClasses = [
@@ -18,7 +18,7 @@ const elClasses = ['w-full', 'max-w-sm', 'mt-4']
 export function useToast(): TailvueToast {
   const outer = document.createElement('div')
   outerClasses.forEach(c => outer.classList.add(c))
-  outer.setAttribute('aria-alive', 'assertive')
+  outer.setAttribute('aria-live', 'assertive')
   const inner = document.createElement('div')
   innerClasses.forEach(c => inner.classList.add(c))
   inner.setAttribute('id', 'toasts')
@@ -26,25 +26,25 @@ export function useToast(): TailvueToast {
   document.body.appendChild(outer)
 
   return {
-    show (props: ToastProps|string) {
+    show(props: ToastProps | string) {
       if (typeof props === 'string') props = { message: props }
       if (props === undefined) return
       return spawn('toasts', props, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
     },
-    success (message: string) {
-      return spawn('toasts', {type: 'success', message}, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
+    success(message: string) {
+      return spawn('toasts', { type: 'success', message }, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
     },
-    info (message: string) {
-      return spawn('toasts', {type: 'info', message}, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
+    info(message: string) {
+      return spawn('toasts', { type: 'info', message }, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
     },
-    danger (message: string) {
-      return spawn('toasts', {type: 'danger', message, timeout: 20}, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
+    danger(message: string) {
+      return spawn('toasts', { type: 'danger', message, timeout: 20 }, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
     },
-    warning (message: string) {
-      return spawn('toasts', {type: 'warning', message, timeout: 10}, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
+    warning(message: string) {
+      return spawn('toasts', { type: 'warning', message, timeout: 10 }, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
     },
-    denied (message: string) {
-      return spawn('toasts', {type: 'denied', message, timeout: 10}, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
+    denied(message: string) {
+      return spawn('toasts', { type: 'denied', message, timeout: 10 }, GlobalToast as unknown as ComponentOptionsWithObjectProps, elClasses)
     },
   }
 
